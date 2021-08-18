92720f814ffd51d36098a3a9650fe4bd4b0e60fb
Rosiethuypham <73338097+Rosiethuypham@users.noreply.github.com>
2021-08-18 23:46:41 +1000

    Add files via upload

--- app.R
+++ app.R
@@ -1,73 +1,106 @@
+#Density paper Study 6
+#Link: https://choicedensity.shinyapps.io/Density_S6/
 
 library(shiny) 
-library(leaflet)
-library(RColorBrewer)
+library(tidyverse)
+library(ggplot2)
+library(readxl)
 
-data <- read.csv("JAMS_S6_Cleaned.csv")
+data <- read_excel("JAMS_S6_Cleaned.xlsx")
 
-ui <- bootstrapPage(
-    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-    leafletOutput("map", width = "100%", height = "100%"),
-    absolutePanel(top = 10, right = 10,
-                  sliderInput("range", "Magnitudes of customer spending", min(data$Spend1.1), max(data$Spend1.1),
-                              value = range(data$Spend1.1), step = 1
+data.den <- dplyr::rename(data, fit = Spend1.1)
+
+Den.lm <- lm(fit ~ Density_SqKm * Choice, data = data.den)
+
+summary(Den.lm)
+
+ui <- fluidPage(
+    
+    titlePanel(
+        title = h2("Population Density Moderates the Impact of Assortment Size on Customer Spending: Model Predictions", align="left"),
+        windowTitle = "Population Density Moderates the Impact of Assortment Size on Customer Spending: Model Predictions"
+    ),
+    
+    sidebarLayout(position = "right",
+                  sidebarPanel(
+                      htmlOutput("predstatement3"),
+                      br(),
+                      sliderInput("Choice", "Product assortment size:",
+                                  min = 6, max = 30,
+                                  value = 18, step = 1),
+                      sliderInput("Density_SqKm", "Population density (people per squared km):",
+                                  min = 5, max = 30000,
+                                  value = 15000, step = 500),
                   ),
-                  selectInput("colors", "Color Scheme",
-                              rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
-                  ),
-                  checkboxInput("legend", "Show legend", TRUE)
+                  mainPanel(
+                      htmlOutput("predstatement1"),
+                      br(),
+                      plotOutput("plot2"),
+                      br(),
+                      br(),
+                      p("The model prediction is generated from Study 6 in the manuscript entitled 'Population Density and Value from Large Assortments'. Under review at the",
+                        span("Journal of the Academy of Marketing Science.", style = "font-style:italic"))
+                  )
     )
 )
 
-server <- function(input, output, session) {
-    
-    # Reactive expression for the data subsetted to what the user selected
-    filteredData <- reactive({
-        data[data$Spend1.1 >= input$range[1] & data$Spend1.1 <= input$range[2],]
+server <- function(input, output){
+    Den.lm <- reactive({
+        Den.lm <- lm(fit ~ Density_SqKm * Choice, data = data.den)
     })
     
-    # This reactive expression represents the palette function,
-    # which changes as the user makes selections in UI.
-    colorpal <- reactive({
-        colorNumeric(input$colors, data$Spend1.1)
+    pred.data <- reactive({
+        pred.data <- data.frame(Density_SqKm = seq(5, 30000, 500), Choice = input$Choice)
     })
     
-    output$map <- renderLeaflet({
-        # Use leaflet() here, and only include aspects of the map that
-        # won't need to change dynamically (at least, not unless the
-        # entire map is being torn down and recreated).
-        leaflet(data) %>% addTiles() %>%
-            fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+    Den.lm.pred <- reactive({
+        Den.lm.pred <- cbind(pred.data(), 
+                             predict(Den.lm(), pred.data(), 
+                                     interval = "confidence", 
+                                     type = c("response", "terms")))
     })
     
-    # Incremental changes to the map (in this case, replacing the
-    # circles when a new color is chosen) should be performed in
-    # an observer. Each independent set of things that can change
-    # should be managed in its own observer.
-    observe({
-        pal <- colorpal()
+    user.pred.data <- reactive({
+        user.pred.data <- data.frame(Density_SqKm = input$Density_SqKm, Choice = input$Choice)
+    })
+    
+    user.pred <- reactive({
+        user.pred <- cbind(user.pred.data(), 
+                           predict(Den.lm(), user.pred.data(), 
+                                   interval = "confidence", 
+                                   type = c("response", "terms")))
+    })
+    
+    user.pred.per.ch <- reactive({
+        user.pred.per.ch <- cbind(user.pred.data(),
+                                  predict(Per.Ch(), user.pred.data(),
+                                          interval = "confidence", 
+                                          type = c("response", "terms")))
         
-        leafletProxy("map", data = filteredData()) %>%
-            clearShapes() %>%
-            addCircles(radius = ~1000*Spend1.1, weight = 1, color = "#777777",
-                       fillColor = ~pal(Spend1.1), fillOpacity = 0.7, popup = ~paste(Spend1.1)
-            )
     })
     
-    # Use a separate observer to recreate the legend as needed.
-    observe({
-        proxy <- leafletProxy("map", data = data)
+    output$predstatement1 <- renderText({
+        print(paste0("<div style = \"font-size:18px\">Based on the input values you selected,the model predicts that customers will spend <b><font color = \"red\">", round(user.pred()$fit, digits = 2),"</font color></b> USD on a pair of headphones.</div>"))
         
-        # Remove any existing legend, and only if the legend is
-        # enabled, create a new one.
-        proxy %>% clearControls()
-        if (input$legend) {
-            pal <- colorpal()
-            proxy %>% addLegend(position = "bottomright",
-                                pal = pal, values = ~Spend1.1
-            )
-        }
     })
+    
+    output$predstatement3 <- renderText({
+        print(paste0("<div style = \"font-size:14px\">Please choose a value on each sliding bar below.</div>"))
+        
+    })
+    
+    output$plot2 <- renderPlot({
+        ggplot(data = data.den, aes(x = Density_SqKm , y = fit)) +
+            xlim(5, 30000) +
+            ylim(0, 200) +
+            geom_segment(data = user.pred(), aes(x = 5, y = fit, xend = Density_SqKm, yend = fit), linetype = "dashed") +
+            geom_segment(data = user.pred(), aes(x = Density_SqKm, y = 0, xend = Density_SqKm, yend = fit), linetype = "dashed") +
+            geom_ribbon(data = Den.lm.pred(), aes(ymin = lwr, ymax = upr), alpha = .3, fill = "gray") +
+            geom_line(data = Den.lm.pred(), color = "black", size = 1) +
+            geom_point(data = user.pred(), color = "red", size = 7) +
+            labs(title="Assortment sizes and Population Density as predictors of Customer Spending", x="Population Density", y="Customer Spending (USD)")
+    })
+    
 }
 
 shinyApp(ui, server)
